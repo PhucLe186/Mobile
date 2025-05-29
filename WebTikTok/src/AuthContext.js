6c2607471e25ca9fd4732f0a2d8b876a7403302a
ttest <hoangphuckaito1806@gmail.com>
2025-05-29 13:03:29 +0700

    29052025

---
+++ WebTikTok/src/AuthContext.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { createContext, useState, useEffect } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:5000/auth/check', { withCredentials: true })
+            .then((res) => setUser(res.data.user))
+            .catch(() => setUser(null));
+    }, []);
+
+    const Login = async (email, password) => {
+        try {
+            const res = await axios.post(
+                'http://localhost:5000/auth/login',
+                { email, password },
+                { withCredentials: true },
+            );
+            setUser(res.data.user);
+        } catch (error) {
+            alert(error.response?.data?.message || 'Đăng nhập thất bại');
+        }
+    };
+
+    const logout = async () => {
+        const res = await axios.post('http://localhost:5000/auth/logout', {}, { withCredentials: true });
+        if (res.data.success) {
+            setUser(null);
+        }
+    };
+
+    return <AuthContext.Provider value={{ user, Login, logout }}>{children}</AuthContext.Provider>;
+};
---
+++ WebTikTok/src/routes/index.js
@@ -0,0 +1,9 @@
+import routesconfig from '~/config/routes';
+
+
+const publicRoutes = [
+    // { path: routesconfig.home, component: home },
+    // { path: routesconfig.hitry, component: BookingHistory, layout: Onlyheader },
+   
+];
+export { publicRoutes };
