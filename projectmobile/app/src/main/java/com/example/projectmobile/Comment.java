75b14f17800bdadfda34f6466d89e55c58792ac8
unknown <chuphuonganh1112@gmail.com>
2025-06-13 21:09:33 +0700

    Bình luận

---
+++ projectmobile/app/src/main/java/com/example/projectmobile/Comment.java
@@ -0,0 +1,20 @@
+package com.example.projectmobile;
+
+public class Comment {
+        private String userName;
+        private String comment;
+
+        public Comment(String userName, String comment) {
+            this.userName = userName;
+            this.comment = comment;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+    }
+
